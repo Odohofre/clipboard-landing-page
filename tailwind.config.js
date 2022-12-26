668984455c17a47454b7b88b68b03210d04fc492
Odohofre <odohofrebright@gmail.com>
2022-12-26 18:45:55 +0100

    update the code to include initial files

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        attribute: '#3e52a3',
+        primary: {
+          300: '#6173ff',
+          800: '#26baa4',
+        },
+        neutral: {
+          400: '#4c545d',
+          800: '#9fabb2',
+        },
+      },
+      backgroundImage: {
+        'header-mobile': "url('./images/bg-header-mobile.png')",
+        'header-desktop': "url('./images/bg-header-desktop.png')",
+      },
+      borderWidth: {
+        '3': '3px',
+      },
+      fontFamily: {
+        Bai: ['"Bai Jamjuree"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
